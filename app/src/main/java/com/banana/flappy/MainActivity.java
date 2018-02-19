f090b673f75f2d495649db99891c7dd3aa85a33e
Banana Android Z <ndrdbnnz@gmail.com>
2018-02-19 20:44:07 +0300

    flappy.2

--- app/src/main/java/com/banana/flappy/MainActivity.java
+++ app/src/main/java/com/banana/flappy/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
 ImageView bg_view;
 ImageView wall_view;
 ImageView man_view;
+TextView scoreTextView;
+TextView scoreViewLose;
+TextView bestRecordView;
+RelativeLayout loseLayout;
 
     float obstacleX = 500;
     float obstacleY = 0;
     float birdY = 0;
     float birdV = 0;
+    int score = 0;
+    int bestRecord = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,13 @@ ImageView man_view;
         setContentView(R.layout.activity_main);
 
         bg_view = findViewById(R.id.bg_view);
+        scoreTextView = findViewById(R.id.score_text_view);
         wall_view = findViewById(R.id.wall_view);
         man_view = findViewById(R.id.bird_view);
+        scoreViewLose = findViewById(R.id.score_view_lose);
+        bestRecordView = findViewById(R.id.best_record_view);
+        loseLayout = findViewById(R.id.lose_layout);
+
         RelativeLayout rootContainer = findViewById(R.id.root_container);
         rootContainer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,12 +72,13 @@ ImageView man_view;
 
         if(wall_view.getTranslationX() < -500){
             obstacleX = 500;
+            score++;
             obstacleY = (float) (Math.random()*400-200);
         }
         birdV += 0.4;
         birdY += birdV;
         if (birdY > 750 || birdY < -750){
-            startNewGame();
+            showGameOverDialog();
         }
 
     }
@@ -73,13 +86,26 @@ ImageView man_view;
     private void onTimerUi() {
         wall_view.setTranslationX(obstacleX);
         wall_view.setTranslationY(obstacleY);
-
+        scoreTextView.setText(String.valueOf(score));
         man_view.setTranslationY(birdY);
+
     }
+    private void showGameOverDialog (){
+        loseLayout.setVisibility(View.VISIBLE);
+        if(score > bestRecord){
+            bestRecord = score;
+        }else(bestRecord >= score){
+            bestRecord = bestRecord;
+        }
+        scoreViewLose.setText("SCORE=" + score);
+        bestRecordView.setText("BEST RECORD=" + bestRecord);
+    }
+
     private void startNewGame(){
         obstacleX = 500;
         obstacleY = 0;
         birdY = 0;
         birdV = 0;
+        score =  0  ;
     }
 }
